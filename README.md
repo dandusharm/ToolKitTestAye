d7f856d4d4992db0c20096dc58e842f17a59eee6
Daniel Dusharm <djdusharm@hotmail.com>
2020-08-02 17:57:13 -0400

    Something is wrong

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdvancedTurnBasedTileTool
+
+Developed with Unreal Engine 4
---
+++ Source/AdvancedTurnBasedTileTool/NewActorComponent.h
@@ -0,0 +1,30 @@
+// Temp
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "NewActorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ADVANCEDTURNBASEDTILETOOL_API UNewActorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UNewActorComponent();
+
+	int sup_dude;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
